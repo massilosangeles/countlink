bf6582fa8b9ee71678444f2b8a713cacef01718b
root <root@localhost.localdomain>
2017-06-14 18:55:09 +0200

    beautifer code

--- count_link.js
+++ count_link.js
@@ -7,11 +7,11 @@ function clean_form(area) {
   if (area == 1) {
     grecaptcha.reset();
   }
-  $('input[name=newsletter]:checked').prop( "checked", false );
-  $('input[name=service]:checked').prop( "checked", false );
+  $('input[name=newsletter]:checked').prop("checked", false);
+  $('input[name=service]:checked').prop("checked", false);
 }
 // A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function() {
   // I do a clean form
   clean_form(2);
   // When form is submit I do ...
@@ -23,15 +23,24 @@ $( document ).ready(function() {
     newsletter = $('input[name=newsletter]:checked').val();
     service = $('input[name=service]:checked').val();
     captcha_response = grecaptcha.getResponse();
-    if (captcha_response != '') {
-      $.post('count_link.php', {site_url: site_url, mail: mail,newsletter: newsletter, service: service, captcha_response: captcha_response }, function(data) {
-      //Write the result
+
+    if (captcha_response != '' && site_url != '' && mail != '' && newsletter != '' && service != '') {
+      $.post('count_link.php', {
+        site_url: site_url,
+        mail: mail,
+        newsletter: newsletter,
+        service: service,
+        captcha_response: captcha_response
+      }, function(data) {
+        //Write the result
         $('#add').show();
         $('#resultat_area').val(data);
         $('#resultat').show();
         clean_form(1);
         $(window).scrollTop($('#resultat').offset().top);
       });
+    } else {
+      alert("Empty input");
     }
     // I give a request to process 
     return false;
@@ -42,7 +51,10 @@ $( document ).ready(function() {
   $('#form_add').submit(function(event) {
     q1 = $('input[name=add1]:checked').val();
     q2 = $('input[name=add2]:checked').val();
-    $.post('add_form.php', {q1: q1, q2: q2}, function(data) {
+    $.post('add_form.php', {
+      q1: q1,
+      q2: q2
+    }, function(data) {
       jQuery.parseJSON(data);
       console.log(data);
       $('#add').hide(); // hide the add after send
--- count_link.php
+++ count_link.php
@@ -2,61 +2,66 @@
 // Function for take content between two delimeter 
 function extract_unit($string, $start, $end)
 {
-$pos = stripos($string, $start);
-
-$str = substr($string, $pos);
-
-$str_two = substr($str, strlen($start));
-
-$second_pos = stripos($str_two, $end);
-
-$str_three = substr($str_two, 0, $second_pos);
-
-$unit = trim($str_three); // remove whitespaces
-
-return $unit;
+  $pos        = stripos($string, $start);
+  $str        = substr($string, $pos);
+  $str_two    = substr($str, strlen($start));
+  $second_pos = stripos($str_two, $end);
+  $str_three  = substr($str_two, 0, $second_pos);
+  $unit       = trim($str_three); // remove whitespaces
+  return $unit;
 }
 // FORM MODE
 extract($_POST);
-// TEST MODE
-//$site_url = "www.example.com";
-// Create a tab with all parti of the url
-$tab_url = explode(".", $site_url);
-$count = 1;
-$intern_link = '';
-$add_http = false;
-// Create the intern url compare
-foreach ($tab_url as $value) {
-	if ($count == sizeof($tab_url)) {
-		if($add_http) {
-			$intern_link = "http://".$intern_link;
-		}
-		break;
-	}
-	if ($count == 1) {
-		if ($value != "http://www") {
-			$add_http = true;
-		}
-	}
-	$intern_link= $intern_link . $value . ".";
-	$count++;
+// Verification of the captcha here ...
+$privatekey = "6LeLTSQUAAAAACX4j177jA6L6Nl5aPraDvyvzuSq";
+$response   = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $privatekey . "&response=" . $captcha_response . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+if ($response == false) {
+  echo 'You are a robot!';
+  
+} else {
+  if ($site_url == '' && $mail == '' && $newsletter == '' && $service == '') {
+    echo "empty input";
+  } else {
+    // TEST MODE
+    //$site_url = "www.example.com";
+    // Create a tab with all parti of the url
+    $tab_url     = explode(".", $site_url);
+    $count       = 1;
+    $intern_link = '';
+    $add_http    = false;
+    // Create the intern url compare
+    foreach ($tab_url as $value) {
+      if ($count == sizeof($tab_url)) {
+        if ($add_http) {
+          $intern_link = "http://" . $intern_link;
+        }
+        break;
+      }
+      if ($count == 1) {
+        if ($value != "http://www") {
+          $add_http = true;
+        }
+      }
+      $intern_link = $intern_link . $value . ".";
+      $count++;
+    }
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $site_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // Write the source page in resultat and close curl
+    $site = curl_exec($ch);
+    curl_close($ch);
+    //Just see in the body
+    $site = extract_unit($site, "<body", "</body>");
+    
+    $links_all    = substr_count($site, "href");
+    $links_intern = substr_count($site, 'href="' . substr($intern_link, 0, -1));
+    $links_intern += substr_count($site, "href='" . substr($intern_link, 0, -1));
+    $link_extern = $links_all - $links_intern;
+    
+    print "There is " . $links_intern . " internal link in that page. And " . $link_extern . " external link";
+  }
 }
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $site_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// Write the source page in resultat and close curl
-$site = curl_exec($ch);
-curl_close($ch);
-//Just see in the body
-$site = extract_unit($site, "<body", "</body>");
-
-$links_all = substr_count($site, "href");
-$links_intern = substr_count($site, 'href="'.substr($intern_link, 0, -1));
-$links_intern += substr_count($site, "href='".substr($intern_link, 0, -1));
-$link_extern = $links_all - $links_intern;
-
-print"There is ".$links_intern." internal link in that page.
-And ".$link_extern." external link";
 
 ?>
